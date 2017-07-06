3e8f526c8c379bc996ee9816ad0e68c690ae0134
lianguanfu <lianguanfu@agrithings.cn>
2017-07-06 19:53:30 +0800

    # WARNING: head commit changed in the meantime
    提交漏了

---
+++ src/main/java/com/dfkj/fcp/core/util/AccessUtils.java
@@ -0,0 +1,25 @@
+package com.dfkj.fcp.core.util;
+
+public class AccessUtils {
+	/**
+	 * 报警服务IP
+	 */
+	public static final String ALARM_SERVICE_HOST = "127.0.0.1";
+	
+	/**
+	 * 报警服务端口
+	 */
+	public static final Integer ALARM_SERVICE_PORT = 8080;
+	
+	
+	/**
+	 * 报警服务请求地址
+	 */
+	public static final String ALARM_SERVICE = String.format("http://%s:%d/terminal/requestAlarm/", ALARM_SERVICE_HOST, ALARM_SERVICE_PORT);
+
+	
+	/**
+	 * 终端上线请求地址
+	 */
+	public static final String ONLINE_SERVICE = String.format("http://%s:%d/terminal/postOnline/", ALARM_SERVICE_HOST, ALARM_SERVICE_PORT);
+}
