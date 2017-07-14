0ce6f1ff934d333feb3908bd0ef904cd25651c37
coLtd <362479845@qq.com>
2017-07-14 13:27:54 +0800

    修复了由于代码不严谨造成的bug

--- src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareCodeFilter.java
+++ src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareCodeFilter.java
@@ -119,7 +119,7 @@ public class HardwareCodeFilter extends IoFilterAdapter {
     private ByteArray dropPack(ByteArray bytes){
 		int i = bytes.size();
 		while(i>3){		  
-		  if(bytes.getBeginByte() != 0x7E && bytes.getAt(18) != 0x7E && bytes.size() >= 18){
+		  if(bytes.getBeginByte() != 0x7E && bytes.getAt(18) != 0x7E && bytes.size() > 18){
 			  bytes.removeAt(0);
 			  i -= 1;
 		  }else{
