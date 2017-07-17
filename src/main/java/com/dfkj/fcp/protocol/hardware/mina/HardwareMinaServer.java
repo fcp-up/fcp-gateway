fa13343941ea2bbafac0d7295b6512e8c6b394ac
coLtd <362479845@qq.com>
2017-07-17 17:41:28 +0800

    增加了离线时终端更新逻辑

--- src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareMinaServer.java
+++ src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareMinaServer.java
@@ -1,22 +1,22 @@
 package com.dfkj.fcp.protocol.hardware.mina;
 
+import java.awt.List;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
-import org.apache.mina.filter.ssl.SslFilter;
+
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import com.dfkj.fcp.config.SystemConfig;
-import com.dfkj.fcp.core.encrypt.SSLContextGenerator;
 import com.dfkj.fcp.core.logger.AcpLogger;
 import com.dfkj.fcp.core.mina.MinaDefaultCodecFactory;
-import com.dfkj.fcp.core.vo.Message;
+import com.dfkj.fcp.core.vo.HelloMessage;
 
 /**
  * Mina硬件服务端
@@ -29,11 +29,8 @@ public class HardwareMinaServer {
     private final static AcpLogger logger = new AcpLogger(HardwareMinaServer.class);
 
     private IoAcceptor acceptor = null;
-
-    /**
-     * 这个消息队列用来接收Mina平台客户端发送的数据
-     */
-    private Map<Integer, LinkedBlockingQueue<Message>> messageQueue = null;
+    
+    Map<Long,HelloMessage> terminalsMap = new HashMap();
 
     public HardwareMinaServer(){
         init();
@@ -41,55 +38,17 @@ public class HardwareMinaServer {
 
     public void init() {
         acceptor = createHardwareAcceptor();
-        messageQueue = new ConcurrentHashMap<Integer, LinkedBlockingQueue<Message>>();
+       
     }
-
-    /**
-     * 是否包含消息
-     *
-     * @param id
-     * @return
-     */
-    public boolean hasMessage(int id) {
-        if (messageQueue.containsKey(id)) {
-            return messageQueue.get(id).size() > 0 ? true : false;
-        }
-        return false;
+    
+    public HelloMessage getTerminalState(long msgId){
+    	return terminalsMap.get(msgId);
+    };
+    
+    public void putTerminalState(long msgId,HelloMessage msg) {
+    	terminalsMap.put(msgId, msg);
     }
-
-    /**
-     * 取出消息
-     *
-     * @param id
-     * @return
-     */
-    public Message take(int id) {
-        if (hasMessage(id)) {
-            try {
-                return messageQueue.get(id).take();
-            } catch (InterruptedException e) {
-                logger.debug(String.format("消息队列中获取消息失败,原因:", e.getMessage()));
-            }
-        }
-        return null;
-    }
-
-    /**
-     * 放入消息
-     *
-     * @param message
-     */
-    public void put(Message message) {
-        if (!hasMessage(message.hashCode())) {
-            messageQueue.put(message.hashCode(), new LinkedBlockingQueue<Message>());
-        }
-        try {
-            messageQueue.get(message.hashCode()).put(message);
-        } catch (InterruptedException e) {
-            logger.debug(String.format("消息队列中添加消息失败，原因:", e.getMessage()));
-        }
-    }
-
+    
     /**
      * 绑定接口,启动Mina服务端
      * @throws Exception
@@ -104,27 +63,14 @@ public class HardwareMinaServer {
     }
 
     /**
-     * 监听硬件Mina服务器的数据,并发给平台客户端
-     *
-     * @param message 消息
-     */
-    public void listenPlatformClientSendMessage(Message message){
-
-        if(message != null){
-            put(message);
-        }
-
-    }
-
-    /**
      * 创建硬件服务器接收对象
      *
      * @return
      */
     private IoAcceptor createHardwareAcceptor() {
         acceptor = new NioSocketAcceptor();
-        SslFilter sslFilter = new SslFilter(new SSLContextGenerator().getSslContext());
-        sslFilter.setUseClientMode(true);
+        /*SslFilter sslFilter = new SslFilter(new SSLContextGenerator().getSslContext());
+        sslFilter.setUseClientMode(true);*/
         acceptor.getSessionConfig().setReadBufferSize(2048);
         acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
 
@@ -137,4 +83,5 @@ public class HardwareMinaServer {
 
         return acceptor;
     }
+	
 }
--- src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareServerHandler.java
+++ src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareServerHandler.java
@@ -2,6 +2,7 @@ package com.dfkj.fcp.protocol.hardware.mina;
 
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,9 +13,10 @@ import org.apache.mina.core.session.IoSession;
 import com.dfkj.fcp.core.constant.EMessageType;
 import com.dfkj.fcp.core.logger.AcpLogger;
 import com.dfkj.fcp.core.util.AccessUtils;
-import com.dfkj.fcp.core.util.ArrayListUtil;
 import com.dfkj.fcp.core.util.HttpRequestUtils;
+import com.dfkj.fcp.core.vo.HelloMessage;
 import com.dfkj.fcp.core.vo.Message;
+import com.dfkj.fcp.protocol.hardware.factory.HardwareInstanceFactory;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -22,6 +24,11 @@ import net.sf.json.JSONObject;
 public class HardwareServerHandler extends IoHandlerAdapter {
 
     private final static AcpLogger logger = new AcpLogger(HardwareServerHandler.class);
+	JSONObject messageObj = new JSONObject();     
+	JSONArray messageOnLineList = new JSONArray();
+	JSONArray messageAlarmList = new JSONArray();
+	String requestOnLineURL = AccessUtils.ONLINE_SERVICE;
+	String requestAlarmLineURL = AccessUtils.ALARM_SERVICE; 
 
     public void messageReceived(IoSession session, Object message) throws Exception {
     		if(!(message instanceof ArrayList)){
@@ -33,18 +40,17 @@ public class HardwareServerHandler extends IoHandlerAdapter {
         		//ArrayListUtil.removeDuplicate(recvMsg); 		
         		int len = recvMsg.size();
         		int i = 0;
-        		JSONArray messageOnLineList = new JSONArray();
-        		JSONArray messageAlarmList = new JSONArray();
-        		String requestOnLineURL = AccessUtils.ONLINE_SERVICE;
-        		String requestAlarmLineURL = AccessUtils.ALARM_SERVICE; 
         		while(i < len){
-        			Message msg = (Message)recvMsg.get(i);    	
-                	JSONObject messageObj = new JSONObject();                	
-                	if(msg.getMsgType() == EMessageType.HELLO){
+        			Message msg = (Message)recvMsg.get(i);   	
+        			if(msg.getMsgType() == EMessageType.HELLO){
                 		messageObj.put("terminalNo", msg.getCenterNo());
                 		messageObj.put("state", 1);
                 		messageObj.put("terminalSignal", msg.getCenterSignal());
-                		messageOnLineList.add(messageObj); 
+                		messageObj.put("stateTime", msg.getRecvMsgDate());
+                		messageOnLineList.add(messageObj);     
+                		logger.info("终端【" + msg.getCenterNo() + "】上线");
+                		HelloMessage helloMsg = new HelloMessage(msg.getCenterNo(),msg.getCenterSignal(),1,msg.getRecvMsgDate());
+                		HardwareInstanceFactory.getMinaServerInstance().terminalsMap.put(session.getId(),helloMsg);
                 	}else if(msg.getMsgType() == EMessageType.SENSOR_DATA){
                 		messageObj.put("terminalNo", msg.getCenterNo());
                 		messageObj.put("deviceNo", msg.getDeviceId());
@@ -56,16 +62,10 @@ public class HardwareServerHandler extends IoHandlerAdapter {
                 	i += 1;
         		}
         		if(messageOnLineList.size()>0){
-        			Map<String,String> reqJsonOnLineParam = new HashMap<>();
-            		reqJsonOnLineParam.put("params", messageOnLineList.toString());
-                	HttpRequestUtils.httpPost(requestOnLineURL,reqJsonOnLineParam);  
-                	messageOnLineList.clear();
+                	reportData(messageOnLineList,requestOnLineURL);
         		}        		
         		if(messageAlarmList.size()>0){
-        			Map<String,String> reqJsonAlarmParam = new HashMap<>();
-                	reqJsonAlarmParam.put("params", messageAlarmList.toString());
-                	HttpRequestUtils.httpPost(requestAlarmLineURL,reqJsonAlarmParam);
-                	messageAlarmList.clear();
+                	reportData(messageAlarmList,requestAlarmLineURL);      	
         		}     		          	
             	recvMsg.clear();            	
         	}catch(Exception e){
@@ -75,5 +75,40 @@ public class HardwareServerHandler extends IoHandlerAdapter {
         		//消息缓存处理    		
         	}    	
     }
+    
+    public void sessionOpened(IoSession session) throws Exception {
+        // Empty handler
+    	HardwareInstanceFactory.getMinaServerInstance().terminalsMap.put(session.getId(), null);
+    }    
+    
+    public void sessionClosed(IoSession session) throws Exception {
+        // Empty handler
+    	HelloMessage msg =  HardwareInstanceFactory.getMinaServerInstance().terminalsMap.get(session.getId());
+    	if(null != msg){
+    		//设置当前终端在线状态
+    		msg.setState(0);
+    		msg.setStateDate(new Date());
+    		logger.info(msg.toString());
+    		//向平台汇报当前状态
+    		logger.info("终端【" + msg.getCenterNo() + "】离线");
+    		messageObj.put("terminalNo", msg.getCenterNo());
+    		messageObj.put("state", msg.getState());
+    		messageObj.put("stateTime", msg.getStateDate());
+    		messageOnLineList.add(messageObj);
+    		reportData(messageOnLineList,requestOnLineURL);
+    	}
+    	logger.info("session " + Long.toString(session.getId()) + " 断开.");
+    	HardwareInstanceFactory.getMinaServerInstance().terminalsMap.remove(session.getId());
+    }
+    
+    //汇报数据
+    public void reportData(JSONArray dataList,String url){
+    	Map<String,String> reqJsonParam = new HashMap<>();
+    	reqJsonParam.put("params", dataList.toString());
+        HttpRequestUtils.httpPost(url,reqJsonParam);
+        dataList.clear();
+    }
+    
+    
     
 }
