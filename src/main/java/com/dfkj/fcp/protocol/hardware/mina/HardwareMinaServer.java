b7596d1230c55f5fd3cd4eb70bd3f9c3f7d8312e
lianguanfu <lianguanfu@agrithings.cn>
2017-07-30 16:14:45 +0800

    增加了离线状态刷新

--- src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareMinaServer.java
+++ src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareMinaServer.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
@@ -16,7 +17,12 @@ import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import com.dfkj.fcp.config.SystemConfig;
 import com.dfkj.fcp.core.logger.AcpLogger;
 import com.dfkj.fcp.core.mina.MinaDefaultCodecFactory;
+import com.dfkj.fcp.core.util.HttpRequestUtils;
 import com.dfkj.fcp.core.vo.HelloMessage;
+import com.dfkj.fcp.core.vo.Message;
+import com.dfkj.fcp.protocol.hardware.factory.HardwareInstanceFactory;
+
+import net.sf.json.JSONArray;
 
 /**
  * Mina硬件服务端
@@ -49,6 +55,20 @@ public class HardwareMinaServer {
     	terminalsMap.put(msgId, msg);
     }
     
+    //汇报数据
+    public void reportData(JSONArray dataList,String url){
+    	Map<String,String> reqJsonParam = new HashMap<>();
+    	reqJsonParam.put("params", dataList.toString());
+        HttpRequestUtils.httpPost(url,reqJsonParam);
+        dataList.clear();
+    }
+    
+    //刷新终端当前状态
+    public void refreshState(Message msg,IoSession session) {
+    	HelloMessage helloMsg = new HelloMessage(msg.getCenterNo(),msg.getCenterSignal(),1,msg.getRecvMsgDate());
+		HardwareInstanceFactory.getMinaServerInstance().terminalsMap.put(session.getId(),helloMsg);
+    }
+    
     /**
      * 绑定接口,启动Mina服务端
      * @throws Exception
@@ -72,8 +92,7 @@ public class HardwareMinaServer {
         /*SslFilter sslFilter = new SslFilter(new SSLContextGenerator().getSslContext());
         sslFilter.setUseClientMode(true);*/
         acceptor.getSessionConfig().setReadBufferSize(2048);
-        acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
-
+        acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 300);
         acceptor.getFilterChain().addLast("codec", new HardwareCodeFilter());
         /*acceptor.getFilterChain().addLast("msg_process", new HardwareMessageFilter());*/
         acceptor.getFilterChain().addFirst("@default_codec@", new ProtocolCodecFilter(new MinaDefaultCodecFactory()));
@@ -82,6 +101,6 @@ public class HardwareMinaServer {
         acceptor.setHandler(new HardwareServerHandler());
 
         return acceptor;
-    }
+    } 
 	
 }
--- src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareServerHandler.java
+++ src/main/java/com/dfkj/fcp/protocol/hardware/mina/HardwareServerHandler.java
@@ -3,17 +3,13 @@ package com.dfkj.fcp.protocol.hardware.mina;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.collections.bag.SynchronizedBag;
 import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import com.dfkj.fcp.core.constant.EMessageType;
 import com.dfkj.fcp.core.logger.AcpLogger;
 import com.dfkj.fcp.core.util.AccessUtils;
-import com.dfkj.fcp.core.util.HttpRequestUtils;
 import com.dfkj.fcp.core.vo.HelloMessage;
 import com.dfkj.fcp.core.vo.Message;
 import com.dfkj.fcp.protocol.hardware.factory.HardwareInstanceFactory;
@@ -50,7 +46,7 @@ public class HardwareServerHandler extends IoHandlerAdapter {
                 		messageObj.put("terminalSignal", msg.getCenterSignal());
 //                		messageObj.put("stateTime", msg.getRecvMsgDate());
                 		messageOnLineList.add(messageObj);    
-                		refreshState(msg,session);
+                		HardwareInstanceFactory.getMinaServerInstance().refreshState(msg,session);
                 		logger.info("集中器【" + msg.getCenterNo() + "】上线.");
                 	}else if(msg.getMsgType() == EMessageType.SENSOR_DATA){
                 		messageObj.put("terminalNo", msg.getCenterNo());
@@ -59,17 +55,17 @@ public class HardwareServerHandler extends IoHandlerAdapter {
                 		messageObj.put("pressure", msg.getVoltage()); 
                 		messageObj.put("deviceSignal", msg.getDeviceSignal());
                 		messageAlarmList.add(messageObj); 
-                		refreshState(msg,session);
+                		HardwareInstanceFactory.getMinaServerInstance().refreshState(msg,session);
                 		logger.info("集中器【" + msg.getCenterNo() + "-" + msg.getDeviceId()  + "】请求报警.");
           		     } 
                 	i += 1;
         		}
         		if(messageOnLineList.size()>0){
-                	reportData(messageOnLineList,requestOnLineURL);
+        			HardwareInstanceFactory.getMinaServerInstance().reportData(messageOnLineList,requestOnLineURL);
         			messageOnLineList.clear();
         		}        		
         		if(messageAlarmList.size()>0){
-                	reportData(messageAlarmList,requestAlarmLineURL); 
+        			HardwareInstanceFactory.getMinaServerInstance().reportData(messageAlarmList,requestAlarmLineURL); 
         			messageAlarmList.clear();
         		}     		          	
             	recvMsg.clear();            	
@@ -91,33 +87,47 @@ public class HardwareServerHandler extends IoHandlerAdapter {
     	HelloMessage msg =  HardwareInstanceFactory.getMinaServerInstance().terminalsMap.get(session.getId());
     	if(null != msg){
     		//设置当前终端在线状态
+    		messageOnLineList.clear();
+    		msg.setState(0);
+    		msg.setStateDate(new Date());
+    		//向平台汇报当前状态
+    		logger.info("集中器【" + msg.getCenterNo() + "】离线.");
+    		messageObj.put("terminalNo", msg.getCenterNo());
+    		messageObj.put("state", msg.getState());
+    		messageOnLineList.add(messageObj);
+    		HardwareInstanceFactory.getMinaServerInstance().reportData(messageOnLineList,requestOnLineURL);
+    	}
+    	logger.info("session " + Long.toString(session.getId()) + " 断开.");
+    }
+    
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+    	HelloMessage msg =  HardwareInstanceFactory.getMinaServerInstance().terminalsMap.get(session.getId());
+    	if(null != msg){
+        	messageOnLineList.clear();
+    		//设置当前终端在线状态
     		msg.setState(0);
     		msg.setStateDate(new Date());
     		logger.info(msg.toString());
     		//向平台汇报当前状态
     		logger.info("集中器【" + msg.getCenterNo() + "】离线.");
     		messageObj.put("terminalNo", msg.getCenterNo());
-    		messageObj.put("state", msg.getState());
-//    		messageObj.put("stateTime", msg.getStateDate());
+    		messageObj.put("state", msg.getState());    		
     		messageOnLineList.add(messageObj);
-    		reportData(messageOnLineList,requestOnLineURL);
+    		HardwareInstanceFactory.getMinaServerInstance().reportData(messageOnLineList,requestOnLineURL);
     	}
-    	logger.info("session " + Long.toString(session.getId()) + " 断开.");
     	HardwareInstanceFactory.getMinaServerInstance().terminalsMap.remove(session.getId());
-    }
-    
-    //汇报数据
-    public void reportData(JSONArray dataList,String url){
-    	Map<String,String> reqJsonParam = new HashMap<>();
-    	reqJsonParam.put("params", dataList.toString());
-        HttpRequestUtils.httpPost(url,reqJsonParam);
-        dataList.clear();
-    }
-    
-    //刷新终端当前状态
-    public void refreshState(Message msg,IoSession session) {
-    	HelloMessage helloMsg = new HelloMessage(msg.getCenterNo(),msg.getCenterSignal(),1,msg.getRecvMsgDate());
-		HardwareInstanceFactory.getMinaServerInstance().terminalsMap.put(session.getId(),helloMsg);
-    }
-    
+    }    
+//    //汇报数据
+//    public void reportData(JSONArray dataList,String url){
+//    	Map<String,String> reqJsonParam = new HashMap<>();
+//    	reqJsonParam.put("params", dataList.toString());
+//        HttpRequestUtils.httpPost(url,reqJsonParam);
+//        dataList.clear();
+//    }
+//    
+//    //刷新终端当前状态
+//    public void refreshState(Message msg,IoSession session) {
+//    	HelloMessage helloMsg = new HelloMessage(msg.getCenterNo(),msg.getCenterSignal(),1,msg.getRecvMsgDate());
+//		HardwareInstanceFactory.getMinaServerInstance().terminalsMap.put(session.getId(),helloMsg);
+//    }
 }
